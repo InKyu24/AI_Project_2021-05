2a61de09f200c8a5ecd4c8b4b2d6dc3d719d4514
InKyu <484342@gmail.com>
2021-03-27 15:13:31 +0900

    [최인규] 최초 프로젝트 생성

---
+++ Bummy/src/main/java/com/bum/BummyApplication.java
@@ -0,0 +1,13 @@
+package com.bum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BummyApplication.class, args);
+	}
+
+}
