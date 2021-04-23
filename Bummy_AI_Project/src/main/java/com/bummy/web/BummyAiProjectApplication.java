0f4462faf0bee9aff71179d22a5852f0c82eaaf1
InKyu <484342@gmail.com>
2021-04-23 18:01:23 +0900

    [최인규] 강의실 생성

--- Bummy_AI_Project/src/main/java/com/bummy/web/BummyAiProjectApplication.java
+++ Bummy_AI_Project/src/main/java/com/bummy/web/BummyAiProjectApplication.java
@@ -2,6 +2,8 @@ package com.bummy.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class BummyAiProjectApplication {
@@ -9,5 +11,9 @@ public class BummyAiProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BummyAiProjectApplication.class, args);
 	}
-
+	
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
 }
