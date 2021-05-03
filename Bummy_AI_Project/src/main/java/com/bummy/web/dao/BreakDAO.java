cb92af0d464cdf086987d419d5d78101c808dd5f
InKyu <484342@gmail.com>
2021-05-03 20:36:11 +0900

    [최인규] 쉬는 시간 설정_DB 연동

---
+++ Bummy_AI_Project/src/main/java/com/bummy/web/dao/BreakDAO.java
@@ -0,0 +1,35 @@
+package com.bummy.web.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.bummy.web.vo.BreakVO;
+
+@Repository
+public class BreakDAO {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	public void breakSet(BreakVO breakVO) {
+		sqlSession.update("mapper.break.breakSet",breakVO);
+	}
+
+	public void signL(BreakVO breakVO) {
+		sqlSession.insert("mapper.break.signL",breakVO);
+		
+	}
+
+	public Boolean breakCheck(BreakVO breakVO) {
+		return sqlSession.selectOne("mapper.break.breakCheck",breakVO);
+	}
+
+	public int breakTimeGet(BreakVO breakVO) {
+		return sqlSession.selectOne("mapper.break.breakTimeGet",breakVO);
+	}
+
+	public String breakTimeMsgGet(BreakVO breakVO) {
+		return sqlSession.selectOne("mapper.break.breakTimeMsgGet",breakVO);
+	}
+}
---
+++ Bummy_AI_Project/src/main/java/com/bummy/web/service/BreakService.java
@@ -0,0 +1,35 @@
+package com.bummy.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bummy.web.dao.BreakDAO;
+import com.bummy.web.vo.BreakVO;
+
+@Service
+public class BreakService {
+	
+	@Autowired
+	BreakDAO breakDAO;
+	
+	public void breakSet(BreakVO breakVO) {
+		breakDAO.breakSet(breakVO);
+	}
+
+	public void signL(BreakVO breakVO) {
+		breakDAO.signL(breakVO);
+		
+	}
+
+	public Boolean breakCheck(BreakVO breakVO) {
+		return breakDAO.breakCheck(breakVO);
+	}
+
+	public int breakTimeGet(BreakVO breakVO) {
+		return breakDAO.breakTimeGet(breakVO);
+	}
+
+	public String breakTimeMsgGet(BreakVO breakVO) {
+		return breakDAO.breakTimeMsgGet(breakVO);
+	}
+}
